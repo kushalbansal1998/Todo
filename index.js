2f5d76749e671c13ee2769d0457b7456f9edadad
Kushal Bansal <kushalbansal1998@gmail.com>
2019-07-23 11:05:36 +0530

    Change in js file

--- index.js
+++ index.js
@@ -1,21 +1,96 @@
-let todo = [];
-
-const list = [];
-
 var input = document.querySelector("input[type = 'text']");
 var ul = document.querySelector("ul");
+let todo = [];
+const tList = [];
+var cc;
 
 input.addEventListener("keypress", function(keyPressed) {
   if (keyPressed.which === 13) {
     var li = document.createElement("li");
-    var element = document.createElement("span");
+    var spanElement = document.createElement("span");
     var icon = document.createElement("i");
+
     var newTodo = this.value;
-    var newTask = { name: newTodo, complete: false };
-    list.push(newTask);
+    var newtask = { name: newTodo, complete: false };
+    tList.push(newtask);
     input.value = "";
     icon.classList.add("fas", "fa-circle");
-    element.append(icon);
-    ul.appendChild(li).append(element, newTodo);
+    spanElement.append(icon);
+    ul.appendChild(li).append(spanElement, newTodo);
+    listRefresh(tList);
   }
 });
+
+const getTodo = showComplete => {
+  if (showComplete == undefined) {
+    todo = tList;
+  } else {
+    todo = tList.filter(({ complete }) => complete === showComplete);
+  }
+
+  listRefresh(todo);
+};
+
+const createTodoFromTemplate = ({ name, complete }, index) => {
+  return `<li class=${complete ? "completed" : "todo"}>
+    <label for="check-checkbox"><i onclick="checkboxtoggle(this,${index})" class='${
+    complete ? "far fa-check-circle" : "fas fa-circle"
+  }'></i></label>
+    <input  id="check-checkbox" class="hide" type="checkbox"/>
+    <label>${name}</label>
+    <button class="remove" onclick="removeCompletedTasks(${index})"></button></li>`;
+};
+
+const listRefresh = tList => {
+  const todosHtml = [];
+  for (i = 0; i < tList.length; i++) {
+    todosHtml.push(createTodoFromTemplate(tList[i], i));
+  }
+  document.getElementById("ilist").innerHTML = todosHtml.join("");
+  taskLeft(tList);
+};
+
+const taskLeft = tList => {
+  var count = 0;
+  for (i = 0; i < tList.length; i++) {
+    if (!tList[i].complete) {
+      count++;
+    }
+  }
+  document.getElementById("iremaining").innerText = count + " item(s) left";
+};
+
+function checkboxtoggle(element, index) {
+  if (element.classList == "fas fa-circle") {
+    element.classList = "far fa-check-circle";
+  } else {
+    element.classList = "fas fa-circle";
+  }
+  toggleList(tList, index);
+  listRefresh(tList);
+}
+
+function toggleList(tList, i) {
+  tList[i].complete = tlist[i].complete ? false : true;
+}
+
+function removeCompletedTasks(index) {
+  tList.splice(index, 1);
+  listRefresh(tList);
+}
+
+function clearAllCompleted() {
+  for (cc of tList) {
+    var activetList = tList.filter(({ complete }) => complete === true);
+
+    var tobedisplayed = tList.filter(function(el) {
+      return activetList.indexOf(el) < 0;
+    });
+    tList.splice(0, tList.length);
+    for (i = 0; i < tobedisplayed.length; i++) {
+      tList[i] = tobedisplayed[i];
+    }
+
+    listRefresh(tList);
+  }
+}
