7d4c532386b4a0c59dd0e84c213e5401dba29e16
Kushal Bansal <kushalbansal1998@gmail.com>
2019-07-23 16:25:59 +0530

    checkbox

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ input.addEventListener("keypress", function(keyPressed) {
     var newtask = { name: newTodo, complete: false };
     tList.push(newtask);
     input.value = "";
-    icon.classList.add("fas", "fa-circle");
+    icon.classList.add("far", "fa-circle");
     spanElement.append(icon);
     ul.appendChild(li).append(spanElement, newTodo);
     listRefresh(tList);
@@ -32,12 +32,12 @@ const getTodo = showComplete => {
 };
 
 const createTodo = ({ name, complete }, index) => {
-  return `<li class=${complete ? "completed" : "todo"}>
-    <label for="check-checkbox"><i onclick="checkboxtoggle(this,${index})" class='${
-    complete ? "far fa-check-circle" : "fas fa-circle"
+  return `<li class=${complete ? "completed" : "userlist"}>
+    <label class="checkboxLabel" for="check-checkbox"><i onclick="checkboxtoggle(this,${index})" class='${
+    complete ? "far fa-check-circle" : "far fa-circle"
   }'></i></label>
     <input  id="check-checkbox" class="hide" type="checkbox"/>
-    <label>${name}</label>
+    <label class="textLabel" >${name}</label>
     <button class="remove fa fa-trash" onclick="removeListTasks(${index})"></button></li>`;
 };
 
@@ -61,10 +61,10 @@ const taskLeft = tList => {
 };
 
 function checkboxtoggle(element, index) {
-  if (element.classList == "fas fa-circle") {
+  if (element.classList == "far fa-circle") {
     element.classList = "far fa-check-circle";
   } else {
-    element.classList = "fas fa-circle";
+    element.classList = "far fa-circle";
   }
   toggleList(tList, index);
   listRefresh(tList);
@@ -79,18 +79,18 @@ function removeListTasks(index) {
   listRefresh(tList);
 }
 
-// function clearCompleted() {
-//   for (cc of tList) {
-//     var activetList = tList.filter(({ complete }) => complete === true);
+function clearCompleted() {
+  for (cc of tList) {
+    var activetList = tList.filter(({ complete }) => complete === true);
 
-//     var display = tList.filter(function(el) {
-//       return activetList.indexOf(el) < 0;
-//     });
-//     tList.splice(0, tList.length);
-//     for (i = 0; i < display.length; i++) {
-//       tList[i] = display[i];
-//     }
+    var display = tList.filter(function(el) {
+      return activetList.indexOf(el) < 0;
+    });
+    tList.splice(0, tList.length);
+    for (i = 0; i < display.length; i++) {
+      tList[i] = display[i];
+    }
 
-//     listRefresh(tList);
-//   }
-// }
+    listRefresh(tList);
+  }
+}
